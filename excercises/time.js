5dfd85ed99214f78696205558afdef7697f9c5cd
Chandrahas Lanka <chandrahaslanka@users.noreply.github.com>
2018-06-30 08:25:15 +0530

    Create time.js

---
+++ excercises/time.js
@@ -0,0 +1,25 @@
+/* Write a function to convert 12 hour time notation to 24 hour time notation
+Input: A string representing 12 hour time in "hh:MM AM/PM" format. 
+Ouptut: A string reresenting equivalent 24 hour time in "HH:MM" format.
+If an invalid input is provided, return the string "INVALID".
+Test Case 1: "01:54 AM" => "01:54"
+Test Case 2: "12:36 PM" => "12:36"
+Test Case 3: "04:16 PM" => "16:16"
+Test Case 4: "16:78 AM" => "INVALID"
+*/
+function twelveToTwentyFour(time) {
+
+}
+
+/* Write a function to convert 24 hour time notation to 12 hour time notation
+Input: A string representing 24 hour time in "HH:MM" format. 
+Ouptut: A string reresenting equivalent 12 hour time in "hh:MM AM/PM" format.
+If an invalid input is provided, return the string "INVALID".
+Test Case 1: "08:23" => "08:23 AM"
+Test Case 2: "00:00" => "12:00 AM"
+Test Case 3: "18:55" => "06:55 PM"
+Test Case 4: "27:88" => "INVALID"
+*/
+function twentyFourToTwelve(time) {
+
+}
